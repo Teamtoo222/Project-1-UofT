b50bd87acb97bf3bb8b6420d1d00433269c32f46
PravTon <clintonsf@outlook.com>
2021-05-30 01:37:25 -0400

    updated the js fetch using user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,55 @@
 // Added the API key on the html with the api call
-// var googleApiKey = "AIzaSyAhOZZGJoUqHE0c14emapGTAXw11nkiHqs";
+
+// Variables
+var searchForm = document.getElementById("searchForm");
+
+cityInput = "Toronto";
+var googleApiKey = "AIzaSyAhOZZGJoUqHE0c14emapGTAXw11nkiHqs";
+var googleGeoCodeUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=" + cityInput + "&key=" + googleApiKey;
+
+
+// submit form event listner 
+searchForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    cityInput = document.getElementById("cityInput").value;
+
+    googleGeoCodeUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=" + cityInput + "&key=" + googleApiKey;
+
+    apiGeoCodeFetch(googleGeoCodeUrl);
+});
+
+// Fetch the google data
+var apiGeoCodeFetch = function(url) {
+    fetch(url)
+    .then(function(response) {
+        if(response.ok) {
+            response.json()
+            .then(function(data) {
+                logPlaceDetails(data);
+                //console.log(data);
+            });
+        }
+    });
+};
 
 
 // Function to pull the nearby the restaurants
-function logPlaceDetails() {
+function logPlaceDetails(passedData) {
+    var latData = passedData.results[0].geometry.location.lat;
+    var lngData = passedData.results[0].geometry.location.lng;
+    keywordImput = document.getElementById("keywordInput").value;
+    //console.log(passedData , latData, lngData);
     var service = new google.maps.places.PlacesService(document.getElementById('map'));
     // Nearby Search method, https://developers.google.com/maps/documentation/javascript/reference/places-service#PlacesService.nearbySearch
     service.nearbySearch({
         // We can use the user input and covert it to lat long and use it here...
-        location: {lat: 43.6532, lng: -79.3832},
+        location: {lat: latData, lng:lngData},
         radius: 500,
         // We can use the keywords field here // could be resaturants or parks 
-        keyword: "restaurants" 
+        keyword: keywordImput 
     },function (place, status) {
         console.log('Place details:', place);
         if(place) {
-            console.log(place.ok)
              for (let i = 0; i < 5; i++) {
                   // Get details method, check this link for more info https://developers.google.com/maps/documentation/javascript/reference/places-service#PlacesService.getDetails
                 service.getDetails({
@@ -32,4 +66,3 @@ function logPlaceDetails() {
 
 };
 
-logPlaceDetails();
