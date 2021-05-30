fcc8a5265eb01c947d96dc0304322d03dcb717d5
Francisco Puma <francis_pancho12@hotmail.com>
2021-05-29 17:57:37 -0700

    limit display to 5 items

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,6 @@ var searchCity = document.querySelector ("#search-city");
 var searchStartDate = document.querySelector ("#search-start-date");
 var searchEndDate = document.querySelector ("#search-end-date");
 var searchButton = document.querySelector ("#search-button");
-var eventsOnPage = document.querySelector ("#events-on-page");
 var currentDate = moment().format('YYYY-MM-DDT04:00:00[Z]');
 var followingDate = moment().add(1, 'days').format('YYYY-MM-DDT04:00:00[Z]');
 
@@ -18,6 +17,8 @@ var detailsContainer = document.querySelector ("#details-container");
 var imgContainer = document.querySelector ("#img-container");
 var eventContainer = document.querySelector ("#event-container");
 var eventCardsContainer = document.querySelector ("#event-cards-container");
+var nearbyEventsSection = document.querySelector ("#nearby-events-section");
+var showMoreEvents = document.querySelector ("#show-more-events");
 
 //var initialSampleAPI = "https://app.ticketmaster.com/discovery/v2/events.json?countryCode=CA&city=Toronto&startDateTime=2021-05-28T04:00:00Z&endDateTime=2021-05-29T04:00:00z&apikey=jYz6ksJAF3WA0eHLAKxbYjp1ZIU0zYlb"
 
@@ -27,7 +28,7 @@ var eventCardsContainer = document.querySelector ("#event-cards-container");
 
 //Function to get event's data
 var getEventData = function() {
-    eventsOnPage.innerHTML = ""
+    eventCardsContainer.innerHTML = ""
     event.preventDefault ();
     var city = searchCity.value
     //var startDate = searchStartDate.value + "T04:00:00Z"// in case we need a value via input
@@ -55,7 +56,7 @@ var getEventData = function() {
                     if (data._embedded != undefined) {
                     var listOfEvents = data._embedded.events
                     console.log (listOfEvents)
-                    displayRecords (listOfEvents);
+                    display5Records (listOfEvents);
                     } else {
                         //alert ("unavailable")
                         //div event-container
@@ -98,9 +99,9 @@ var getEventData = function() {
         searchCity.value = ""
 }
 
-// Function to display the data 
- var displayRecords = function(listOfEvents) {
-   for (i = 0; i < listOfEvents.length ; i++) {
+// Function to display the initial 5 data 
+ var display5Records = function(listOfEvents) {
+   for (i = 0; i < 5 ; i++) {
 
        // var from array from API
        var eventName = listOfEvents[i].name;
@@ -146,7 +147,6 @@ var getEventData = function() {
        pEventULR.id = "event-url";
        pEventULR.innerHTML = "<a href =" + eventUrl + " >Details </a>"
 
-
        //appending information
        divEventDetails.appendChild (pEventName);
        divEventDetails.appendChild (pEventTime);
@@ -154,9 +154,101 @@ var getEventData = function() {
        divDetailsContainer.appendChild (divEventDetails);
        divEventsContainer.appendChild (divDetailsContainer);
        eventCardsContainer.appendChild (divEventsContainer);
-       
+             
     }
+
+    //Showmore button
+    var showMoreEventsBtn = document.createElement ("button");
+    showMoreEventsBtn.className = "button is-medium is-danger is-light show-more-button is-flex mt-5";
+    showMoreEventsBtn.id = "show-more-events"
+    showMoreEventsBtn.innerHTML = "Show More"
+    nearbyEventsSection.appendChild (showMoreEventsBtn);
+    debugger
+
+    //showMoreEvents.addEventListener("click", displayAllRecords (listOfEvents));
+   //displayAllRecords (listOfEvents);
+    
+
+    
  }
+
+
+// Function to display the initial remaining data 
+// var displayAllRecords = function(listOfEvents) {
+//     debugger
+//     nearbyEventsSection.innerHTML = ""
+//     debugger
+//     nearbyEventsSection.innerHTML = 
+//     "<div class='title-container column has-text-centered'>" +
+//         "<h3 class='title is-size-3 mt-3 mb-2'>Nearby Events</h3>" +
+//         "<h5 class='title is-size-5 pt-2 sub-heading'>Location</h5>" +
+//     "</div>" +
+//     "<div class='cards-container columns is-full pt-5 is-flex-wrap-wrap' id = 'event-cards-container'>"+
+//     "<div>"
+
+//     for (i = 0; i < listOfEvents.length ; i++) {
+ 
+//         // var from array from API
+//         var eventName = listOfEvents[i].name;
+//         var eventLocalTime = listOfEvents[i].dates.start.localTime;
+//         var eventUrl = listOfEvents[i].url;
+//         //var eventLocalDate = listOfEvents[i].dates.start.localDate // variable to add the local time found in the array if needed
+ 
+//         //div event-container
+//         var divEventsContainer = document.createElement ("div");
+//         divEventsContainer.className = "event-container column card p-0";
+//         divEventsContainer.id = "event-container";
+//         divEventsContainer.innerHTML = 
+//         "<div class='img-container'id = 'img-container'>" + 
+//         "<div class='store-status is-flex is-justify-content-flex-end'>" +
+//         "</div>" +
+//         "</div>";
+ 
+//         //div details-container
+//         var divDetailsContainer = document.createElement ("div");
+//         divDetailsContainer.className = "details-container";
+//         divDetailsContainer.id = "details-container";
+ 
+//         //div event-details
+//         var divEventDetails = document.createElement ("div");
+//         divEventDetails.className = "store-details is-flex";
+//         divEventDetails.id = "event-details";
+ 
+//         //p for event name
+//         var pEventName = document.createElement ("p");
+//         pEventName.className = "name";
+//         pEventName.id = "event-name";
+//         pEventName.innerHTML = "<strong>" + eventName + "</strong>"
+ 
+//         //p for event time
+//         var pEventTime = document.createElement ("p");
+//         pEventTime.className = "time";
+//         pEventTime.id = "event-time";
+//         pEventTime.innerHTML = eventLocalTime 
+ 
+//         //p for event url
+//         var pEventULR = document.createElement ("p");
+//         pEventULR.className = "url";
+//         pEventULR.id = "event-url";
+//         pEventULR.innerHTML = "<a href =" + eventUrl + " >Details </a>"
+ 
+//         //appending information
+//         divEventDetails.appendChild (pEventName);
+//         divEventDetails.appendChild (pEventTime);
+//         divEventDetails.appendChild (pEventULR);
+//         divDetailsContainer.appendChild (divEventDetails);
+//         divEventsContainer.appendChild (divDetailsContainer);
+//         eventCardsContainer.appendChild (divEventsContainer);
+              
+//     }
+// }
+
+
+
+
+
+
+
 
 //Event listener
 searchButton.addEventListener("click",getEventData);
