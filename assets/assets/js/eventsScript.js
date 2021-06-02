3ca75461e7d2bd2a4c16e1ba9e93237d5c2d1f86
Farnoush <farnoush.yousefikejani@uwaterloo.ca>
2021-06-02 14:23:45 -0400

    test

---
+++ assets/assets/js/eventsScript.js
@@ -0,0 +1,271 @@
+//General variables
+
+var APIkey = "jYz6ksJAF3WA0eHLAKxbYjp1ZIU0zYlb";
+var searchCity = document.querySelector ("#search-city");
+var searchStartDate = document.querySelector ("#search-start-date");
+var searchEndDate = document.querySelector ("#search-end-date");
+var searchButton = document.querySelector ("#search-button");
+// added the form id 
+var searchForm = document.querySelector("#searchForm");
+var currentDate = moment().format('YYYY-MM-DDT08:00:00[Z]');
+var followingDate = moment().add(1, 'days').format('YYYY-MM-DDT07:59:00[Z]');
+var localArray = []
+
+//Variables for display cards
+var eventName = document.querySelector ("#event-name");
+var eventTime = document.querySelector ("#event-time");
+var eventURL = document.querySelector ("#event-url");
+var eventDetails = document.querySelector ("#event-details");
+var detailsContainer = document.querySelector ("#details-container");
+var imgContainer = document.querySelector ("#img-container");
+var eventContainer = document.querySelector ("#event-container");
+var eventCardsContainer = document.querySelector ("#event-cards-container");
+var nearbyEventsSection = document.querySelector ("#nearby-events-section");
+var showMoreEvents = document.querySelector ("#show-more-events");
+var showMoreEventsBtn = document.querySelector ("#show-more-events-btn");
+
+//var initialSampleAPI = "https://app.ticketmaster.com/discovery/v2/events.json?countryCode=CA&city=Toronto&startDateTime=2021-05-28T04:00:00Z&endDateTime=2021-05-29T04:00:00z&apikey=jYz6ksJAF3WA0eHLAKxbYjp1ZIU0zYlb"
+
+
+
+//ALL Functions
+
+//Function to get event's data
+var getEventData = function() {
+    eventCardsContainer.innerHTML = ""
+    showMoreEvents.innerHTML =""
+    event.preventDefault ();
+    var city = searchCity.value
+    console.log(city)
+
+
+    if (city === "") {
+        alert("please insert value")
+    } else {
+    //var startDate = searchStartDate.value + "T04:00:00Z"// in case we need a value via input
+    //var endDate = searchEndDate.value + "T04:00:00Z"// in case we need a value via input
+    //saveData(city,startDate,endDate);//possible local storage function if needed
+
+    var eventsAPI = "https://app.ticketmaster.com/discovery/v2/events.json?countryCode=CA&size=15&sort=relevance,desc&city=" 
+                + city 
+                + "&startDateTime=" 
+                + currentDate 
+                + "&endDateTime="
+                + followingDate 
+                + "&apikey="
+                + APIkey
+
+    console.log (eventsAPI);
+
+    fetch (eventsAPI)
+        .then(function(response) {
+            if (response.ok) {
+                response.json().then(function (data){
+                    console.log (data);// all details
+                    //console.log (data._embedded.events);// list of all events
+                    //console.log (data._embedded.events[0].name);// name of the first event
+                    if (data._embedded != undefined || data._embedded != null || city.value != null) {
+                    var listOfEvents = data._embedded.events
+                    console.log (listOfEvents)
+                    display5Records (listOfEvents);
+                    } else {
+                        //alert ("unavailable")
+                        //div event-container
+                        var divEventsContainer = document.createElement ("div");
+                        divEventsContainer.className = "event-container column card p-0";
+                        divEventsContainer.id = "event-container";
+
+                        //div details-container
+                        var divDetailsContainer = document.createElement ("div");
+                        divDetailsContainer.className = "details-container";
+                        divDetailsContainer.id = "details-container";
+
+                        //div event-details
+                        var divEventDetails = document.createElement ("div");
+                        divEventDetails.className = "store-details is-flex";
+                        divEventDetails.id = "event-details";
+
+                        //p for Alert for nothing available
+                        var pnothing = document.createElement ("p");
+                        pnothing.className = "is-size-3 has-text-centered";
+                        pnothing.id = "event-name";
+                        pnothing.innerHTML = "<strong> Nothing is Available </strong>"
+
+
+                        //appending information
+                        divEventDetails.appendChild (pnothing);
+                        divDetailsContainer.appendChild (divEventDetails);
+                        divEventsContainer.appendChild (divDetailsContainer);
+                        eventCardsContainer.appendChild (divEventsContainer);
+       
+                    }
+
+                })
+            }
+        })
+        .catch(function(error) {
+            alert("Unable to connect");
+        })
+    }
+
+        searchCity.value = ""
+}
+
+// Function to display the initial 5 data 
+ var display5Records = function(listOfEvents) {
+    
+   for (i = 0; i < 5 ; i++) {
+
+       // var from array from API
+       var eventName = listOfEvents[i].name;
+       var img = listOfEvents[i].images[7].url;
+       //console.log(img);
+       var eventLocalTime = listOfEvents[i].dates.start.localTime;
+       var eventUrl = listOfEvents[i].url;
+       //var eventLocalDate = listOfEvents[i].dates.start.localDate // variable to add the local time found in the array if needed
+
+       //div event-container
+       var divEventsContainer = document.createElement ("div");
+       divEventsContainer.className = "event-container column card p-0";
+       divEventsContainer.id = "event-container";
+       divEventsContainer.innerHTML = 
+       "<div class='img-container' style = 'background-image: url("+img+")' id = 'img-container'>"+
+       "<div class='store-status is-flex is-justify-content-flex-end'>" +
+       "</div>" +
+       "</div>";
+
+       //div details-container
+       var divDetailsContainer = document.createElement ("div");
+       divDetailsContainer.className = "details-container";
+       divDetailsContainer.id = "details-container";
+
+       //div event-details
+       var divEventDetails = document.createElement ("div");
+       divEventDetails.className = "store-details is-flex";
+       divEventDetails.id = "event-details";
+
+       //p for event name
+       var pEventName = document.createElement ("p");
+       pEventName.className = "name";
+       pEventName.id = "event-name";
+       pEventName.innerHTML = "<strong>" + eventName + "</strong>"
+
+       //p for event time
+       var pEventTime = document.createElement ("p");
+       pEventTime.className = "time";
+       pEventTime.id = "event-time";
+       pEventTime.innerHTML = eventLocalTime 
+
+       //p for event url
+       var pEventULR = document.createElement ("p");
+       pEventULR.className = "url";
+       pEventULR.id = "event-url";
+       pEventULR.innerHTML = "<a href =" + eventUrl + " >Details </a>"
+
+       //appending information
+       divEventDetails.appendChild (pEventName);
+       divEventDetails.appendChild (pEventTime);
+       divEventDetails.appendChild (pEventULR);
+       divDetailsContainer.appendChild (divEventDetails);
+       divEventsContainer.appendChild (divDetailsContainer);
+       eventCardsContainer.appendChild (divEventsContainer);
+             
+    }
+
+    //Showmore button  
+
+    var showMoreEventsBtn = document.createElement ("button");
+    showMoreEventsBtn.className = "button is-medium is-danger is-light show-more-button is-flex mt-5";
+    showMoreEventsBtn.id = "show-more-events-btn"
+    showMoreEventsBtn.innerHTML = "Show More"
+
+    showMoreEvents.appendChild (showMoreEventsBtn)
+    
+    // localArray =[]
+    // localArray.push(listOfEvents)
+    // console.log (localArray);
+    //debugger
+
+    $ ("#show-more-events-btn").click(function() {
+    displayAllRecords(listOfEvents);
+    })
+ }
+
+
+// Function to display the initial remaining data 
+var displayAllRecords = function(listOfEvents) {
+
+    // $ ("#show-more-events-btn").click(function(localArray) {
+    eventCardsContainer.innerHTML = ""
+    showMoreEvents.innerHTML =""
+    console.log (listOfEvents)
+   //debugger
+
+    for (i = 0; i < listOfEvents.length ; i++) {
+ 
+        // var from array from API
+        var eventName = listOfEvents[i].name;
+        var eventLocalTime = listOfEvents[i].dates.start.localTime;
+        var eventUrl = listOfEvents[i].url;
+        var img = listOfEvents[i].images[7].url;
+        //var eventLocalDate = listOfEvents[i].dates.start.localDate // variable to add the local time found in the array if needed
+ 
+        //div event-container
+        var divEventsContainer = document.createElement ("div");
+        divEventsContainer.className = "event-container column card p-0";
+        divEventsContainer.id = "event-container";
+        divEventsContainer.innerHTML = 
+        "<div class='img-container' style = 'background-image: url("+img+")' id = 'img-container'>"+ 
+        "<div class='store-status is-flex is-justify-content-flex-end'>" +
+        "</div>" +
+        "</div>";
+ 
+        //div details-container
+        var divDetailsContainer = document.createElement ("div");
+        divDetailsContainer.className = "details-container";
+        divDetailsContainer.id = "details-container";
+ 
+        //div event-details
+        var divEventDetails = document.createElement ("div");
+        divEventDetails.className = "store-details is-flex";
+        divEventDetails.id = "event-details";
+ 
+        //p for event name
+        var pEventName = document.createElement ("p");
+        pEventName.className = "name";
+        pEventName.id = "event-name";
+        pEventName.innerHTML = "<strong>" + eventName + "</strong>"
+ 
+        //p for event time
+        var pEventTime = document.createElement ("p");
+        pEventTime.className = "time";
+        pEventTime.id = "event-time";
+        pEventTime.innerHTML = eventLocalTime 
+ 
+        //p for event url
+        var pEventULR = document.createElement ("p");
+        pEventULR.className = "url";
+        pEventULR.id = "event-url";
+        pEventULR.innerHTML = "<a href =" + eventUrl + " >Details </a>"
+ 
+        //appending information
+        divEventDetails.appendChild (pEventName);
+        divEventDetails.appendChild (pEventTime);
+        divEventDetails.appendChild (pEventULR);
+        divDetailsContainer.appendChild (divEventDetails);
+        divEventsContainer.appendChild (divDetailsContainer);
+        eventCardsContainer.appendChild (divEventsContainer);
+              
+    }
+};
+    // )}
+
+
+
+
+
+
+// Change the function to submit so it works on the other side as well.
+//Event listener
+searchForm.addEventListener("submit",getEventData);
+//showMoreEvents.addEventListener("click", displayAllRecords (localArray)); //problem lies here remove parameter 
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,8 @@
 // Variables
 var searchForm = document.getElementById('searchForm');
 
-cityInput = 'Toronto';
+var cityInput = 'Toronto';
+var keywordInput = '';
 var googleApiKey = 'AIzaSyAhOZZGJoUqHE0c14emapGTAXw11nkiHqs';
 var googleGeoCodeUrl =
   'https://maps.googleapis.com/maps/api/geocode/json?address=' +
@@ -11,112 +12,371 @@ var googleGeoCodeUrl =
   '&key=' +
   googleApiKey;
 
+var placeArray = [];
+var errorModal = document.getElementById('error-Modal');
+
+let iStart = 0;
+let iEnd = 5;
+let type = 'restaurant';
+var targetId = '#nearby-resturants';
+var mainCont = document.getElementById('mainContainer');
+var heroContainer = document.getElementById('heroContainer');
+
+// type = 'tourist_attraction';
+// targetId = '#nearby-recreation';
+
+var service = new google.maps.places.PlacesService(
+  document.getElementById('map')
+);
 // submit form event listner
 searchForm.addEventListener('submit', function (event) {
   event.preventDefault();
   cityInput = document.getElementById('search-city').value;
-  googleGeoCodeUrl =
-    'https://maps.googleapis.com/maps/api/geocode/json?address=' +
-    cityInput +
-    '&key=' +
-    googleApiKey;
+  keywordInput = document.getElementById('keywordInput').value;
 
-  apiGeoCodeFetch(googleGeoCodeUrl);
+  if (!cityInput) {
+    errorModal.classList.remove('hideMsg');
+    errorModal.classList.add('showMsg');
+  } else {
+    mainCont.style.display = 'block';
+    document.getElementById('error-Modal').classList.add('hideMsg');
+    errorModal.classList.remove('showMsg');
+    googleGeoCodeUrl =
+      'https://maps.googleapis.com/maps/api/geocode/json?address=' +
+      cityInput +
+      '&key=' +
+      googleApiKey;
+
+    iStart = 0;
+    iEnd = 5;
+
+    var selectedOption = document.getElementById('selectOption').value;
+    var covidContainer = document.getElementById('covidContainer');
+    var eventsContainer = document.getElementById('nearby-events-section');
+    var restaurantsContainer = document.getElementById('restaurantsContainer');
+    var recreationContainer = document.getElementById('recreationContainer');
+
+    //document.getElementById("mainContainer").innerHTML = "";
+
+    heroContainer.classList.remove('hero-def-height');
+    mainCont.classList.add('p-2', 'main-container');
+
+    if (selectedOption === 'Events') {
+      apiGeoCodeFetch(googleGeoCodeUrl, selectedOption);
+      covidContainer.classList.remove('hideEl');
+      covidContainer.classList.add('showEl');
+      recreationContainer.classList.add('hideEl');
+      recreationContainer.classList.remove('columns');
+      eventsContainer.classList.remove('hideEl');
+      eventsContainer.classList.add('columns');
+      eventsContainer.classList.add('showEl');
+    } else if (selectedOption === 'Restaurants') {
+      eventsContainer.classList.add('hideEl');
+      eventsContainer.classList.remove('columns');
+      covidContainer.classList.remove('hideEl');
+      covidContainer.classList.add('showEl');
+      recreationContainer.classList.add('hideEl');
+      recreationContainer.classList.remove('columns');
+      restaurantsContainer.classList.remove('hideEl');
+      restaurantsContainer.classList.add('columns');
+      document.getElementById('nearby-resturants').innerHTML = '';
+      document.getElementById('resLocation').textContent = cityInput;
+      apiGeoCodeFetch(googleGeoCodeUrl, selectedOption);
+    } else if (selectedOption === 'Recreations') {
+      type = 'tourist_attraction';
+      targetId = '#nearby-recreation';
+      eventsContainer.classList.add('hideEl');
+      eventsContainer.classList.remove('columns');
+      restaurantsContainer.classList.add('hideEl');
+      restaurantsContainer.classList.remove('columns');
+      covidContainer.classList.remove('hideEl');
+      covidContainer.classList.add('showEl');
+      recreationContainer.classList.remove('hideEl');
+      recreationContainer.classList.add('columns');
+      document.getElementById('nearby-recreation').innerHTML = '';
+      document.getElementById('recLocation').textContent = cityInput;
+      apiGeoCodeFetch(googleGeoCodeUrl, selectedOption);
+    }
+
+    // searchForm.reset();
+    document.querySelector('#keywordInput').value = '';
+  }
 });
 
-let resCount = 5;
-let recCount = 5;
-
 // Fetch the google data
-var apiGeoCodeFetch = function (url) {
+var apiGeoCodeFetch = function (url, option) {
+  console.log('url', url);
+  console.log('option', option);
   fetch(url).then(function (response) {
     if (response.ok) {
       response.json().then(function (data) {
-        logPlaceDetails(data, 'restaurant', '#nearby-resturants', resCount);
-        logPlaceDetails(data, 'recreation', '#nearby-recreation', recCount);
-        console.log('DATAAAAA', data);
-        document.querySelector('#show-rec').addEventListener('click', () => {
-          recCount += 5;
-          document.querySelector('#nearby-recreation').innerHTML = '';
-          logPlaceDetails(data, 'recreation', '#nearby-recreation', recCount);
-        });
+        if (option === 'Events') {
+          covidLoc(data);
+        } else {
+          covidLoc(data);
+          logResPlaceDetails(data);
+        }
 
-        document.querySelector('#show-res').addEventListener('click', () => {
-          resCount += 5;
-          document.querySelector('#nearby-resturants').innerHTML = '';
-          logPlaceDetails(data, 'restaurant', '#nearby-resturants', resCount);
-        });
+        // logPlaceDetails(data, 'parks');
+        console.log('DATAAAAA', data);
       });
     }
   });
 };
 
 // Function to pull the nearby the restaurants
-function logPlaceDetails(passedData, type, target, number) {
+function logResPlaceDetails(passedData) {
   var latData = passedData.results[0].geometry.location.lat;
   var lngData = passedData.results[0].geometry.location.lng;
-  keywordInput = document.getElementById('keywordInput').value;
   //console.log(passedData , latData, lngData);
-  var service = new google.maps.places.PlacesService(
-    document.getElementById('map')
-  );
   // Nearby Search method, https://developers.google.com/maps/documentation/javascript/reference/places-service#PlacesService.nearbySearch
   service.nearbySearch(
     {
       // We can use the user input and covert it to lat long and use it here...
       location: { lat: latData, lng: lngData },
-      radius: 5000,
+      radius: 10000,
       // We can use the keywords field here
       keyword: keywordInput,
       // I think we could use the same function for recreations as we will be able to change the type and get this to work
       type: type,
     },
     function (place, status) {
-      //   console.log('Place details:', place);
-      if (place) {
-        for (let i = 0; i < number; i++) {
-          // Get details method, check this link for more info https://developers.google.com/maps/documentation/javascript/reference/places-service#PlacesService.getDetails
-          if (place[i]) {
-            service.getDetails(
-              {
-                placeId: place[i].place_id,
-              },
-              function (place, status) {
-                console.log('rec details **** :', place);
-                if (place) {
-                  const template = `
-                  <a href=${place.url}>
-                  <div class="event-container column card p-0">
-                  <div class="img-container" style="background-image:url(${place.photos[0].getUrl()});">
-                  <div class="store-status is-flex is-justify-content-flex-end">
-                  <p class="open-status">OPEN</p>
-                  </div>
-                  </div>
-                  <div class="details-container">
-                  <div class="store-details is-flex">
-                  <p class="store-name"><strong>${place.name}</strong> </p>
-                    <p class="store-address">${place.formatted_address}</p>
-                    <p class="store-status">${place.business_status}</p>
-                    <p class="store-phone">${place.formatted_phone_number}</p>
-                    <a href=${place.website}>Details</a>
-                    </div>
-                    </div>
-                    </div>
-                    </a>
-                    `;
-
-                  const container = document.createElement('div');
-                  container.innerHTML = template;
-                  document.querySelector(target).append(container);
-                }
-              }
-            );
-          }
-          //Retrive Photo Urls
-          var placePhotoUrl = place[i].photos[0].getUrl({ maxWidth: 640 });
-          console.log(placePhotoUrl);
-        }
-      }
+      console.log('Place details:', place);
+      placeArray = place;
+      passNearByData(placeArray);
     }
   );
+}
+
+var passNearByData = function (place) {
+  if (place) {
+    for (let i = iStart; i < iEnd; i++) {
+      // Get details method, check this link for more info https://developers.google.com/maps/documentation/javascript/reference/places-service#PlacesService.getDetails
+      if (place[i]) {
+        console.log(place[i]);
+        service.getDetails(
+          {
+            placeId: place[i].place_id,
+          },
+          function (place, status) {
+            // console.log("this is the error status", status);
+            if (place) {
+              console.log('rec details **** :', place);
+              createCards(place);
+            }
+          }
+        );
+      }
+      //Retrive Photo Urls
+      // var placePhotoUrl = place[i].photos[0].getUrl({ maxWidth: 640 });
+      // console.log(placePhotoUrl);
+    }
+  }
+};
+
+var createCards = function (place) {
+  // console.log(place.opening_hours.isOpen());
+  // Check if store has opening hours
+  if (place.hasOwnProperty('opening_hours')) {
+    if (place.opening_hours.isOpen() === true) {
+      var openStatus = 'Open Now';
+    } else {
+      openStatus = 'Closed Now';
+    }
+  } else {
+    openStatus = 'Please Call ';
+  }
+
+  // Check to see if the business is operational
+  if (place.business_status === 'OPERATIONAL') {
+    var businesStatus = 'Operational';
+    var busClassList = 'busStatus bg-green';
+    // document.querySelector(".busStatus").style.background = "green";
+  } else if (place.business_status === 'CLOSED_TEMPORARILY') {
+    businesStatus = 'Temporarily closed';
+    busClassList = 'busStatus bg-red';
+  } else {
+    businesStatus = 'Permanently closed';
+    busClassList = 'busStatus bg-red';
+  }
+
+  const template = `
+    <a class="linkImage" href=${place.url} target="_blank">
+    <div class="img-container" style="background-image:url(${place.photos[0].getUrl()});">
+    <div class="store-status is-flex is-align-items-flex-end is-flex-direction-column">
+    <div class="w-100 is-flex is-justify-content-space-between">
+    <p class="ratingNumb"><i class="ratingNumbIcon fas fa-star-half-alt"></i>&nbsp${
+      place.rating
+    }</p>
+    <p class="mb-2 open-status">${openStatus}</p>
+    </div>
+    <p class="${busClassList}">${businesStatus}</p>
+    </div>
+    </div>
+    </a>
+    <div class="details-container">
+    <div class="store-details is-flex">
+    <p class="store-name wrap-content"><i class="fas fa-bars"></i>&nbsp<strong>${
+      place.name
+    }</strong></p>
+      <a class="wrap-content" href="https://maps.google.com/maps?q=${
+        place.formatted_address
+      }" target="_blank class="store-address"><i class="fas fa-map-marker-alt">&nbsp</i>${
+    place.formatted_address
+  }</a>
+      <a href="tel:${
+        place.formatted_phone_number
+      }" class="store-phone"><i class="fas fa-phone-alt"></i>&nbsp${
+    place.formatted_phone_number
+  }</a>
+      <a href="${
+        place.website
+      }" target="_blank"><i class="fas fa-globe"></i>&nbsp Website</a>
+      </div>
+      </div>
+      `;
+
+  const container = document.createElement('div');
+  container.classList = 'event-container column card p-0';
+  container.innerHTML = template;
+  document.querySelector(targetId).append(container);
+};
+
+var busStatChecker = function () {
+  // Change the background color of busStats
+  var busStatEl = document.querySelectorAll('.busStatus');
+
+  for (var i = 0; i < busStatEl.length; i++) {
+    console.log('test');
+    if (busStatEl[i] === 'Operational') {
+      var statEl = document.querySelector('.busStatus');
+      statEl.classList.remove('bg-red');
+      statEl.classList.add('bg-green');
+    }
+  }
+};
+
+document.querySelector('#show-res').addEventListener('click', () => {
+  iStart += 5;
+  iEnd += 5;
+  //document.querySelector('#nearby-resturants').innerHTML = '';
+  //logPlaceDetails(data, 'recreation', '#nearby-recreation', recCount);
+  passNearByData(placeArray);
+});
+
+document.querySelector('#show-rec').addEventListener('click', () => {
+  iStart += 5;
+  iEnd += 5;
+  //document.querySelector('#nearby-resturants').innerHTML = '';
+  //logPlaceDetails(data, 'recreation', '#nearby-recreation', recCount);
+  passNearByData(placeArray);
+});
+
+var currentDay = moment().subtract(1, 'day').format('DD-MM-YYYY');
+// var province = "ON";
+console.log(currentDay);
+
+var covidLoc = function (data) {
+  var addressData = data.results[0].address_components;
+  var province = '';
+  if (addressData.length === 8) {
+    province = addressData[addressData.length - 3].short_name;
+  } else {
+    province = addressData[addressData.length - 2].short_name;
+  }
+  var covidUrl =
+    'https://api.opencovid.ca/summary?loc=' + province + '&date=' + currentDay;
+  console.log(province);
+  console.log(covidUrl);
+  covidData(covidUrl);
+};
+
+var covidArray = [];
+
+// Fetch the google data
+var covidData = function (covidUrl) {
+  fetch(covidUrl).then(function (response) {
+    if (response.ok) {
+      response.json().then(function (data) {
+        // logPlaceDetails(data, 'parks');
+        console.log('DATAAAAA', data);
+        saveCovidData(data);
+        displayCovidStats(data);
+      });
+    }
+  });
+};
+
+var saveCovidData = function (data) {
+  localStorage.setItem('covidData', JSON.stringify(data));
+};
+
+var loadCovidData = function () {
+  loadedData = JSON.parse(localStorage.getItem('covidData'));
+
+  if (!loadedData) {
+    return;
+  } else {
+    covidArray = loadedData;
+
+    console.log('this is the returned array', covidArray);
+
+    displayCovidStats(covidArray);
+  }
+};
+
+var totalCases = document.querySelector('#totalCases');
+var totalDeaths = document.querySelector('#totalDeaths');
+var totalRecovered = document.querySelector('#totalRecovered');
+var totalTests = document.querySelector('#totalTests');
+var totalActive = document.querySelector('#totalActive');
+var totalVaccine = document.querySelector('#totalVaccine');
+var todayCases = document.querySelector('#todayCases');
+var todayDeaths = document.querySelector('#todayDeaths');
+var todayRecovered = document.querySelector('#todayRecovered');
+var todayTests = document.querySelector('#todayTests');
+var todayChange = document.querySelector('#todayChange');
+var todayVaccine = document.querySelector('#todayVaccine');
+var proviceEl = document.querySelectorAll('.province');
+var covidLocEl = document.querySelector('#covidLocation');
+
+function formatNumber(num) {
+  return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,');
+}
+
+var displayCovidStats = function (covidArray) {
+  totalCases.textContent = formatNumber(covidArray.summary[0].cumulative_cases);
+  totalDeaths.textContent = formatNumber(
+    covidArray.summary[0].cumulative_deaths
+  );
+  totalRecovered.textContent = formatNumber(
+    covidArray.summary[0].cumulative_recovered
+  );
+  todayCases.textContent = formatNumber(covidArray.summary[0].cases);
+  todayDeaths.textContent = formatNumber(covidArray.summary[0].deaths);
+  todayRecovered.textContent = formatNumber(covidArray.summary[0].recovered);
+  covidLocEl.textContent = formatNumber(covidArray.summary[0].province);
+  totalTests.textContent = formatNumber(
+    covidArray.summary[0].cumulative_testing
+  );
+  totalActive.textContent = formatNumber(covidArray.summary[0].active_cases);
+  totalVaccine.textContent = formatNumber(
+    covidArray.summary[0].cumulative_avaccine
+  );
+  todayTests.textContent = formatNumber(covidArray.summary[0].testing);
+  todayChange.textContent = formatNumber(
+    covidArray.summary[0].active_cases_change
+  );
+  todayVaccine.textContent = formatNumber(covidArray.summary[0].avaccine);
+};
+
+loadCovidData();
+
+if (placeArray.length !== 0 || covidArray.length !== 0) {
+  console.log(placeArray);
+  mainCont.style.display = 'block';
+  heroContainer.classList.remove('hero-def-height');
+  mainCont.classList.add('p-2', 'main-container');
+} else {
+  mainCont.style.display = 'none';
 }
